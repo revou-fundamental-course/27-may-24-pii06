a055d3bef9888786233eef49017ec32cd81c34f4
Rafii <rafiistarleo1@gmail.com>
2024-06-06 15:18:21 +0800

    Commit 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-//* Ini Javascript //*
+document.addEventListener("DOMContentLoaded", function () {
+    const nameSpan = document.getElementById("name!");
+    const name = prompt("Nama anda: ", "");
+    
+    if (name) { nameSpan.textContent = name; } });
